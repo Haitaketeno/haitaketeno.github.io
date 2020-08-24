3e27f939978b191ae761f126df5dbc0061f9161e
Richard Rossmann <rossmann.richard@gmail.com>
2020-08-24 13:58:51 +1000

    updated homepage

--- googlechart.js
+++ googlechart.js
@@ -3,22 +3,23 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
 var data = google.visualization.arrayToDataTable([
-  ['event', 'person one', 'person two', 'point of no return'],
-  ['start of unlucky run',  480,      520,		0],
-  ['continuing decline',  180,      220,		0],
-  ['we\'ve hit the bottom',  -20,      20,		0],
-  ['recovery, but not for all',  -50,      240,		0],
-  ['more ups & downs',  -50,      130,		0],
-  ['one can thrive again, the other is fucked',  -50,      380,		0]
+  ['event', 'person 1 no safety net', 'person 1 safety net', 'person two', 'point of no return'],
+  ['start of unlucky run',  520,	,      580,		0],
+  ['continuing decline',  200,		,      260,		0],
+  ['we\'ve hit the bottom',  0,	0,      40,		0],
+  ['recovery, but not for all',  -50,	60,      240,		0],
+  ['more ups & downs',  -50, 	20,     130,		0],
+  ['one can thrive again, the other is fucked',  -50, 	270,     380,		0]
 ]);
 
 var options = {
   title: 'gambler\'s ruin',
   curveType: 'function',
-  legend: { position: 'bottom' },
+  legend: { position: 'top', maxLines: 3 },
   series: { 0:{color: '#B61219'},
-			1:{color: 'dimgrey'},
-			2:{ color: 'grey', lineDashStyle: [2,2]} 
+			1:{color: 'navy'},
+			2:{color: 'dimgrey'},
+			3:{ color: 'grey', lineDashStyle: [2,2]} 
 			},
   vAxis: {
     gridlines: {
